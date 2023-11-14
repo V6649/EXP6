b3e806b547879eb7e359b9dfed0450f68a7ee35b
ViswaDhanushReddy <Viswa234587@gmail.com>
2023-11-15 00:52:40 +0530

    f

---
+++ src/app/Services/enroll.service.ts
@@ -0,0 +1,7 @@
+export class EnrollService
+{
+    OnEnrollClicked(title: string)
+    {
+            alert('Thanking for enrolling to '+title+'course.');
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularComponent } from './angular/angular.component';
+import { JavascriptComponent } from './javascript/javascript.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent,
+    JavascriptComponent
   ],
   imports: [
     BrowserModule,
